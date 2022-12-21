7e7c6898433c442ae69ebeef3dbd6e0b3b74f417
Emily Berger <excaliberger@gmail.com>
2022-12-21 12:12:26 -0500

    reworded a step

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
-`1. Decide which business to order from.
-2. Decide which type of pizza to order.`
+`
+1. Decide which business to order from.
+    1a. see if they have any specials.
+2. Decide which type of pizza to order.
+3. Make sure order, address, and phone number are correct on their end.
+`
